08f3bb36387261372040239fbee8c3857aab2380
MinskyNg <minsky199525@gmail.com>
2016-09-25 23:24:46 +0800

    重写简历

--- src/js/resume.js
+++ src/js/resume.js
@@ -1,48 +1,11 @@
-(function($) {
-    $(document).ready(function() {
-        $('#loading').fadeOut();
-        $('#container').fullpage({
-            navigation: true,
-            navigationTooltips: ['自我介绍', '前端基础', '其他技能', '练习作品', '联系方式'],
-            css3: true,
-            afterLoad: function(anchorLink, index) {
-                var loadedSection = $(this);
+//弹出微信二维码
+function showAddWechat(){
+    document.getElementById('addWechat').style.display='block';
+}
 
-                if (index == 1) {
-                    loadedSection.find('h1').removeClass('hidden').addClass('animated swing');
-                    loadedSection.find('h2').removeClass('hidden').addClass('animated fadeInUp');
-                    loadedSection.find('h3').removeClass('hidden').addClass('animated fadeInUp');
-                }
+//关闭微信二维码
+function closeAddWechat(){
+    this.style.display='none';
+}
 
-                if (index == 2) {
-                    loadedSection.find('h1').removeClass('hidden').addClass('animated fadeIn');
-                    loadedSection.find('p').removeClass('hidden').addClass('animated fadeInUp');
-                    $('.progress-bar span').eq(0).addClass('progress-html');
-                    $('.progress-bar span').eq(1).addClass('progress-css');
-                    $('.progress-bar span').eq(2).addClass('progress-js');
-                }
-
-                if (index == 3) {
-                    loadedSection.find('h1').removeClass('hidden').addClass('animated fadeIn');
-                    loadedSection.find('h2').removeClass('hidden').addClass('animated tada');
-                    loadedSection.find('p').removeClass('hidden').addClass('animated fadeInUp');
-                }
-
-                if (index == 4) {
-                    loadedSection.find('h1').removeClass('hidden').addClass('animated fadeIn');
-                    loadedSection.find('img').removeClass('hidden').addClass('animated zoomIn');
-                    loadedSection.find('h2').removeClass('hidden');
-                    loadedSection.find('p').removeClass('hidden').addClass('animated fadeInUp');
-                }
-
-                if (index == 5) {
-                    loadedSection.find('h1').removeClass('hidden').addClass('animated fadeIn');
-                    loadedSection.find('h2').removeClass('hidden').addClass('animated fadeInUp');
-                    loadedSection.find('h3').removeClass('hidden').addClass('animated fadeInDown');
-                    loadedSection.find('p').removeClass('hidden').addClass('animated fadeInUp');
-                }
-            }
-        });
-        $('#fp-nav span').css('background-color', '#fff');
-    });
-})(jQuery);
+document.getElementById('addWechat').onclick = closeAddWechat;
